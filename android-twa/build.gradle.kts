7719a48ed5d6f1d5d1cf35233d5dd69eeb4785c0
Forever322 <Forever322@users.noreply.github.com>
2026-08-05 01:16:38 +0800

    fix: 降级 Compose BOM 和 Android Gradle 插件版本以解决兼容性问题，更新样式以优化视觉效果

--- android-twa/build.gradle.kts
+++ android-twa/build.gradle.kts
@@ -1,6 +1,6 @@
 // 顶级构建文件：在此声明插件版本，供子模块按需应用
 plugins {
-    id("com.android.application") version "8.12.0" apply false
+    id("com.android.application") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     // Compose 编译器插件版本必须与 Kotlin 插件版本完全一致
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
